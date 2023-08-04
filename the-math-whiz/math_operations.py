2a0f75835dd5d4f71d2c9e72bae3c5c04440e8d1
a.kumuyi <a.kumuyi@alustudent.com>
2023-08-04 11:33:30 +0000

    This file contains methods for mathematical operations.

---
+++ the-math-whiz/math_operations.py
@@ -0,0 +1,21 @@
+# math_operations.py
+
+import math
+
+class MathWhiz:
+    # Other methods from the original MathWhiz class...
+
+    @staticmethod
+    def round_two_decimal(num):
+        """
+        Rounds the number to two decimal places.
+
+        :param num: The number to be rounded.
+        :type num: float
+        :return: The rounded number.
+        :rtype: float
+        """
+        return round(num, 2)
+
+    # Other math operation-related methods from the original MathWhiz class...
+
